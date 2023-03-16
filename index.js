a27fed6e2760da19f1b38cf749a465a01d191344
Jeremy Kaplan <jeremykaplan10@gmail.com>
2023-03-16 17:26:28 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+}
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+        debugger
+    }
+    return gifts
+}
+wrapGifts(gifts)
+
+function writeCards(cards, string) {
+    const array = []
+    for (let i = 0; i < cards.length; i++) {
+        console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+        array.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+    return array
+    
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i)
+    }
+}
+console.log(countDown(10))
